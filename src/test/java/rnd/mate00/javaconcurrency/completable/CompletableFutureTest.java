2f358d5a3446c599a394895b85fbca4dec043b4d
mate00 <mami.ext@gmail.com>
2020-05-21 07:13:41 +0200

    Intro to CompletableFuture.

---
+++ src/test/java/rnd/mate00/javaconcurrency/completable/CompletableFutureTest.java
@@ -0,0 +1,32 @@
+package rnd.mate00.javaconcurrency.completable;
+
+import org.junit.Test;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+
+public class CompletableFutureTest {
+
+    @Test
+    public void simpleCase() throws ExecutionException, InterruptedException {
+        CompletableFuture<String> future = new CompletableFuture<>();
+
+        Executors.newSingleThreadExecutor().submit(() -> {
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            future.complete("result of computations");
+        });
+
+        System.out.println(future.get());
+    }
+
+    @Test
+    public void noNeedToWait() throws ExecutionException, InterruptedException {
+        CompletableFuture<String> completed = CompletableFuture.completedFuture("we know the result already");
+        System.out.println(completed.get());
+    }
+}
