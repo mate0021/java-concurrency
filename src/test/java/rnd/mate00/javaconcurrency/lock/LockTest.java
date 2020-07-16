1fc3ab1691e5328e1a971583235db8ff198d4e47
mate00 <mami.ext@gmail.com>
2020-07-16 09:04:45 +0200

    Lock. Object with state and lock.

--- src/test/java/rnd/mate00/javaconcurrency/lock/LockTest.java
+++ src/test/java/rnd/mate00/javaconcurrency/lock/LockTest.java
@@ -2,13 +2,16 @@ package rnd.mate00.javaconcurrency.lock;
 
 import org.junit.Test;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class LockTest {
 
     @Test
-    public void useThisIdion() {
+    public void useThisIdiom() {
         Lock lock = new ReentrantLock();
 
         lock.lock();
@@ -18,5 +21,41 @@ public class LockTest {
             System.out.println("Whatever happens, unlock it.");
             lock.unlock();
         }
+    }
+
+    @Test
+    public void nonSynchronizedSharedObject() throws InterruptedException {
+        SharedObject shared = new SharedObject();
+
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        for (int i = 0; i < 10; i++) {
+            executorService.execute(() -> {
+                try {
+                    System.out.println(Thread.currentThread() + " " + shared.incrementAndGet());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            } );
+        }
+
+        executorService.shutdown();
+        executorService.awaitTermination(5, TimeUnit.SECONDS);
+
+    }
+}
+
+class SharedObject {
+    private int counter;
+    private Lock lock = new ReentrantLock();
+
+    int incrementAndGet() throws InterruptedException {
+        lock.lock();
+        try {
+            Thread.sleep(10);
+            counter++;
+        } finally {
+            lock.unlock();
+        }
+        return counter;
     }
 }
